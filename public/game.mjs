ad3d97abc46eb5584de70b05018bc4d0f2f96e17
Todd Libby <toddlibby@protonmail.com>
2025-10-05 15:44:55 -0700

    Socket.io issue to resolve

--- public/game.mjs
+++ public/game.mjs
@@ -1,6 +1,6 @@
 import Player from './Player.mjs';
 import Collectible from './Collectible.mjs';
 
-const io = require('socket.io')(http);
+const socket = require('socket.io')(http);
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
