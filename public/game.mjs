cfd03506cf18e088e915226bfbec5975b720ab7f
Todd Libby <toddlibby@protonmail.com>
2025-10-06 11:02:27 -0700

    Fixing stuff

--- public/game.mjs
+++ public/game.mjs
@@ -1,131 +1,136 @@
 import Player from './Player.mjs';
 import Collectible from './Collectible.mjs';
-import { dimension } from './dimension.mjs';
+
+//Variables Declaration
 const socket = io();
-
-
-let tick;
-let playersList = [];
-let oxygenEntity;
-let playerEntity;
-let spikeEntity;
-
-
 const canvas = document.getElementById('game-window');
 const context = canvas.getContext('2d');
+const mainPlayer = new Player({x: Math.floor(Math.random() * (canvas.width-10)),y: Math.floor(Math.random() * (canvas.height-10)),score: 0,id: getRandomId()})
+
+var collectible
+var direction = ""
+var players = []
+var ownrank = 0
+const height = 10
+const speed = 20
+var canCatch = true
+var emittedCollectible = false
+
+//Loading images at initializationg for quicker operation later
+var collectibleDrawingGold = new Image();
+    collectibleDrawingGold.src = 'https://github.com/LaurentLabine/fcc_secure_real_time_multiplayer_game/raw/master/public/Images/gold-coin.png'; // can also be a remote URL e.g. http://
+var collectibleDrawingSilver = new Image();
+    collectibleDrawingSilver.src = 'https://github.com/LaurentLabine/fcc_secure_real_time_multiplayer_game/raw/master/public/Images/silver-coin.png'; // can also be a remote URL e.g. http://
+var collectibleDrawingBronze = new Image();
+    collectibleDrawingBronze.src = 'https://github.com/LaurentLabine/fcc_secure_real_time_multiplayer_game/raw/master/public/Images/bronze-coin.png'; // can also be a remote URL e.g. http://
+var mainPlayerDrawing = new Image();
+    mainPlayerDrawing.src = 'https://github.com/LaurentLabine/fcc_secure_real_time_multiplayer_game/raw/master/public/Images/main-player.png'
+var otherPlayerDrawing = new Image();
+    otherPlayerDrawing.src = 'https://github.com/LaurentLabine/fcc_secure_real_time_multiplayer_game/raw/master/public/Images/other-player.png'
+
+socket.emit('newPlayer', mainPlayer);
 
 
-let meImage = new Image();
-let otherImage = new Image();
-let oxygenImage = new Image();
-let spikeImage = new Image();
-
-const init = () => {
-  // get images
-  meImage.src = 'public/img/green.png';
-  otherImage.src = 'public/img/white.png';
-  oxygenImage.src = 'public/img/oxygen.png';
-  spikeImage.src = 'public/img/spikedball.png';
-  
-  // create user
-  socket.on('init', ({ id, players, oxygen, spike }) => {
-    console.log(id, players,oxygen,spike);
-    oxygenEntity = new Collectible(oxygen);
-    playerEntity = players.filter(x => x.id === id)[0];
-    playerEntity = new Player(playerEntity);
-    spikeEntity = new Player(spike);
-  
-    playersList = players
-
-
-    document.onkeydown = e => {
-      let  dir = null
-      switch(e.keyCode) {
-        case 87:
-        case 38:
-           dir = 'up';
-           break;
-        case 83:
-        case 40:
-           dir = 'down';
-           break;
-        case 65:
-        case 37:
-           dir = 'left';
-           break;
-        case 68:
-        case 39:
-           dir = 'right';
-           break;   
-      }
-      if (dir) {
-        playerEntity.movePlayer(dir, 10);
-        socket.emit('update', playerEntity);
-      }
-    }
-  
-    // update
-    socket.on('update', ({players:players,spike:spike,oxygen:oxygen,player:player}) => {
-      spikeEntity = new Player(spike)
-      playersList = players;
-      oxygenEntity = new Collectible(oxygen)
-      if (player) {
-        if (player.id === playerEntity.id) {
-          playerEntity= new Player(player);
+const render = (gameState) => {
+    context.clearRect(0, 0, 640, 480);
+    //Drawing collectible
+    if(gameState.collectible){
+        switch(gameState.collectible.value){
+            case 1:
+            context.drawImage(collectibleDrawingBronze, gameState.collectible.x, gameState.collectible.y)
+            break;
+            case 2:
+            context.drawImage(collectibleDrawingSilver, gameState.collectible.x, gameState.collectible.y)
+            break
+            case 3:
+            context.drawImage(collectibleDrawingGold, gameState.collectible.x, gameState.collectible.y)
+            break;
+            default:
+                console.log("Collectible Exception Detected")
+            break
         }
-      }
-      
-    });
-  
-  });
-  
-  window.requestAnimationFrame(update); 
+    }
+
+    //Drawing players
+    for (let player in gameState.players)
+        if(gameState.players[player].id == mainPlayer.id)
+            context.drawImage(mainPlayerDrawing, gameState.players[player].x, gameState.players[player].y)
+        else
+            context.drawImage(otherPlayerDrawing, gameState.players[player].x, gameState.players[player].y)
+
+    //Drawing Rank
+    context.font = "40pt Calibri";
+    context.fillText(ownrank, 400, 50);
 }
 
-const update = () => {
-
-  context.clearRect(0, 0, canvas.width, canvas.height);
-
-  // Set background color
-  context.fillStyle = '#1c4966';
-  context.fillRect(0, 0, canvas.width, canvas.height);
-
-  // Create border for play field
-  context.strokeStyle = '#45b6fe';
-  context.strokeRect(dimension.minX, dimension.minY, dimension.arenaSizeX, dimension.arenaSizeY);
-
-  // Controls text
-  context.fillStyle = '#45b6fe';
-  context.font = `13px 'Press Start 2P'`;
-  context.textAlign = 'center';
-  context.fillText('Controls', 80, 20);
-  context.textAlign = 'center';
-  context.fillText('WASD', 80, 40);
-
-  // Game title
-  context.font = `40px 'Modak'`;
-  context.fillText('Bubble survivor', 300, 40);
-
-  if (playerEntity) {
-    playerEntity.draw(context,meImage);
-    context.font = `26px 'Modak'`;
-    context.fillText(playerEntity.calculateRank(playersList), 560, 40);
-    playersList.forEach((player)=> {
-       if (player.id !== playerEntity.id) {
-         let p = new Player(player);
-         p.draw(context, otherImage);
-       }
-    });
-    if (oxygenEntity) {
-      oxygenEntity.draw(context,oxygenImage);
+function getRandomInt(max) {
+    return Math.floor(Math.random() * Math.floor(max));
     }
-    if (spikeEntity) {
-      spikeEntity.draw(context,spikeImage);
+
+socket.on("state", (gameState)=>{
+    if(!gameState)
+        return
+    
+    if(!gameState.collectible && gameState.lobbyLeader == mainPlayer.id && !emittedCollectible){//if no collectible, emit a new one
+        socket.emit("newCollectible", new Collectible({x:getRandomInt(canvas.width-40),y: getRandomInt(canvas.height-40), value: getRandomInt(3)+1, id: getRandomId()}))
+        emittedCollectible = true
+
+        //Adding a delay to prevent firing new collectible twice in between completion of new collectible event
+        setInterval(()=> {
+            emittedCollectible = false}
+            , 1000)
     }
+    else
+        collectible = gameState.collectible
+
+    if(players.length)
+        ownrank = mainPlayer.calculateRank(players)
+
+    if(collectible)
+        if(mainPlayer.collision(collectible) && canCatch){
+            socket.emit("collectibleCaught", {id: mainPlayer.id, value: collectible.value})
+            canCatch = false
+            //Adding a delay to prevent firing collectible caught twice in between caught and event ran
+            setInterval(()=> {
+                canCatch = true}
+                , 1000)
+        }
+
+    //Was faced with either reorganizing my players collection on server side as an array instead of a collection of object or implement a "patch"
+    //to maintain my players array.  Opted for this as it makes sense for me at this time to keep a socket ID collection on server side.
+    if(players.length != gameState.nbPlayers){ 
+        players = []
+        for (let player in gameState.players)
+            players.push(gameState.players[player])
+    }
+    render(gameState)
+})
+
+const keyDownHandler = (e) => {
+
+    if(e.keyCode == 39 && mainPlayer.x < 640)
+        direction = "right"
+    else if (e.keyCode == 37 && mainPlayer.x > 0)
+        direction = "left"
+    else if (e.keyCode == 38 && mainPlayer.y > 0)
+        direction = "up"
+    else if(e.keyCode == 40 && mainPlayer.y < 480- height)
+    direction = "down"
+    
+    mainPlayer.movePlayer(direction,speed)
+}
+
+function getRandomId() {
+    var letters = '0123456789ABCDEF';
+    var str = '';
+    for (var i = 0; i < 6; i++) {
+      str += letters[Math.floor(Math.random() * 16)];
+    }
+    return str;
   }
 
- 
-  tick = requestAnimationFrame(update);
-}
+setInterval(()=> {
+    socket.emit("playerMoved", mainPlayer)}
+    , 1000 /60)
 
-init();
+document.addEventListener('keydown', keyDownHandler, false);
